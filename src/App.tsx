93e17a9a956845700a8109ccbd648b007d56b2f4
ronlv4 <ronlv4@gmail.com>
2021-03-20 11:22:10 +0200

    Created the first component - Square

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Square from './components/Square'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Square/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Square.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Square (){
+    return(
+        <div>
+            <h1>check</h1>
+        </div>
+    )
+}
+
+export default Square
