b84d90f002d1f8ff424f07ced32f656f16aa00a0
ronlv4 <ronlv4@gmail.com>
2021-04-02 19:19:25 +0300

    grid on screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-import BigSquare from './components/BigSquare'
+import produce from 'immer';
 
 
-export type AppProps = {
-  numOfRows: number,
-  numOfCols: number
-}
+  const numOfRows = 50
+  const numOfCols = 50
 
-export type AppState = {
-	board: JSX.Element[]
-}
+// export type AppState = {
+// 	board: number
+// }
 
 
-export class App extends React.PureComponent<{}, AppState> {
+
+// export class App extends React.PureComponent<{}, AppState> {
+  const App: React.FC = () =>{
+    const [grid,setgrid] = useState(()=>{
+      const rows=[]
+      for(let i=0;i<numOfRows;i++){
+        rows.push(Array.from(Array(numOfCols),()=>0))
+      }
+      return rows;
+    })
+
+    console.log(grid);
+
+    return(
+      <div style = {{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numOfCols}, 20px)`
+      }}>
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy =>{
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                })
+                setgrid(newGrid)
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? "black" : undefined,
+                border: "solid 1px black"
+              }}
+            />
+          ))
+        )}
+      </div>
+    )
 
   // props: AppProps = {
   //   numOfRows: 10,
   //   numOfCols: 15
   // }
+  // state: AppState = {
+  //   number: 5
+  // }
 
-	state: AppState = {
-		board: [<BigSquare/>,<BigSquare/>,<BigSquare/>]
-	}
+	// state: AppState = {
+	// 	board: [<BigSquare/>,<BigSquare/>,<BigSquare/>]
+	// }
 
-  render(){
-
-    // const game = this.state.board.map(()=> return <)
-    
-    return (
-      <div className="App">
-        <BigSquare/>
-      </div>
-    );
-  }
 }
 
 export default App;
