29c76d2494a2ca66511b319e7549ae8b3488bbc9
ronlv4 <ronlv4@gmail.com>
2021-04-02 23:10:44 +0300

    grid, toggle mouseclick, toggle mousemove drawing

--- src/App.tsx
+++ src/App.tsx
@@ -3,51 +3,75 @@ import './App.css';
 import produce from 'immer';
 
 
-  const numOfRows = 50
-  const numOfCols = 50
+  const numOfRows = 30
+  const numOfCols = 30
 
 // export type AppState = {
 // 	board: number
 // }
 
-
-
-// export class App extends React.PureComponent<{}, AppState> {
-  const App: React.FC = () =>{
-    const [grid,setgrid] = useState(()=>{
-      const rows=[]
+const createEmptyGrid = ()=>{
+  const rows=[]
       for(let i=0;i<numOfRows;i++){
         rows.push(Array.from(Array(numOfCols),()=>0))
       }
       return rows;
-    })
+}
 
-    console.log(grid);
+const createRandomizedGrid = ()=>{
+  const rows=[]
+      for(let i=0;i<numOfRows;i++){
+        rows.push(Array.from(Array(numOfCols),()=> Math.random()>0.99? 1 : 0))
+      }
+      return rows;
+}
 
+// export class App extends React.PureComponent<{}, AppState> {
+  const App: React.FC = () =>{
+    const [grid,setgrid] = useState(()=>{return createEmptyGrid()})
     return(
-      <div style = {{
-        display: 'grid',
-        gridTemplateColumns: `repeat(${numOfCols}, 20px)`
-      }}>
-        {grid.map((rows, i) =>
-          rows.map((col, k) => (
-            <div
-              key={`${i}-${k}`}
-              onClick={() => {
-                const newGrid = produce(grid, gridCopy =>{
-                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
-                })
-                setgrid(newGrid)
-              }}
-              style={{
-                width: 20,
-                height: 20,
-                backgroundColor: grid[i][k] ? "black" : undefined,
-                border: "solid 1px black"
-              }}
-            />
-          ))
-        )}
+      <div>
+        <button
+        onClick={()=>{setgrid(createRandomizedGrid())}}>
+          randomize grid
+        </button>
+        <button
+        onClick={()=>{setgrid(createEmptyGrid())}}>
+          Clear Grid
+        </button>
+
+        <div style = {{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${numOfCols}, 20px)`
+        }}>
+          {grid.map((rows, i) =>
+            rows.map((col, k) => (
+              <div
+                key={`${i}-${k}`}
+                onMouseEnter={(e)=>{
+                  if(e.buttons===1){
+                    const newGrid = produce(grid, gridCopy =>{
+                      gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                    })
+                    setgrid(newGrid)
+                  }
+                }}
+                onClick={() => {
+                  const newGrid = produce(grid, gridCopy =>{
+                    gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                  })
+                  setgrid(newGrid)
+                }}
+                style={{
+                  width: 20,
+                  height: 20,
+                  backgroundColor: grid[i][k] ? "black" : undefined,
+                  border: "solid 1px black"
+                }}
+              />
+            ))
+          )}
+        </div>
       </div>
     )
 
